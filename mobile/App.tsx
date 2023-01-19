dff06001572d0650608514dd68da2e1d644a32cc
Elesiann <giovanimcorrea18@gmail.com>
2023-01-19 18:25:54 -0300

    dayjs import

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,16 @@
-import { StatusBar } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
 import {
-  useFonts,
   Poppins_200ExtraLight,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_600SemiBold,
   Poppins_700Bold,
   Poppins_800ExtraBold,
+  useFonts,
 } from "@expo-google-fonts/poppins";
+import { StatusBar } from "react-native";
 import { Loading } from "./src/components/Loading";
 import { Home } from "./src/screens/Home";
+import "./src/lib/dayjs";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
