f8fb35fb2afffb0b6031a43ac6a0e7830bcf6865
Elesiann <giovanimcorrea18@gmail.com>
2023-01-21 11:58:26 -0300

    [Front] Creating checkboxes

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -3,6 +3,8 @@ import * as Popover from "@radix-ui/react-popover";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
 import dayjs from "dayjs";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { Check } from "phosphor-react";
 
 interface HabitProps {
   date: Date;
@@ -41,6 +43,18 @@ export function Habit({ completed = 0, amount = 0, date }: HabitProps) {
         <span className="mt-1 font-bold leading-tight text-3xl">{"20/01"}</span>
 
         <ProgressBar progress={completedPercentage} />
+
+        <Checkbox.Root className="flex items-center gap-3 group focus:outline-none mt-4">
+          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-charcoal border-2 border-blackCoral group-data-[state=checked]:bg-blue-500 group-data-[state=checked]:border-blue-300 transition-all">
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
+
+          <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+            {"test"}
+          </span>
+        </Checkbox.Root>
 
         <Popover.Arrow width={20} height={10} className="fill-gunmetal" />
       </Popover.Content>
--- web/src/components/HabitForm.tsx
+++ web/src/components/HabitForm.tsx
@@ -1,8 +1,38 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
 import { Check } from "phosphor-react";
+import { FormEvent, useState } from "react";
 
 export function HabitForm() {
+  const [formValues, setFormValues] = useState<string>("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const availableWeekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+    console.log(formValues);
+  }
+
+  function handleToggleCheckbox(weekDay: number) {
+    weekDays?.includes(weekDay)
+      ? setWeekDays(weekDays.filter((day: number) => day !== weekDay))
+      : setWeekDays([...weekDays, weekDay]);
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form
+      onSubmit={(e) => createNewHabit(e)}
+      className="w-full flex flex-col mt-6"
+    >
       <label htmlFor="title" className="font-semibold leading-tight">
         What's your commitment?
       </label>
@@ -13,11 +43,28 @@ export function HabitForm() {
         id="title"
         placeholder="ex.: Exercise, 8 hours of sleep..."
         autoFocus
+        onChange={(e) => setFormValues(e.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         What's the frequency?
       </label>
+
+      {availableWeekDays.map((day, index) => (
+        <Checkbox.Root
+          key={day}
+          className="flex items-center gap-3 group focus:outline-none mt-2"
+          onCheckedChange={() => handleToggleCheckbox(index)}
+        >
+          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-charcoal border-2 border-blackCoral group-data-[state=checked]:bg-blue-500 group-data-[state=checked]:border-blue-300 transition-all">
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
+
+          <span className="text-white leading-tight">{day}</span>
+        </Checkbox.Root>
+      ))}
 
       <button
         type="submit"
